e96f8ecfbb671c289de5eaac98b9bb82eacf470c
ALEYI <aleelaleyi@gmail.com>
2024-08-19 13:36:26 -0500

    Ya esta hecho9 para conseguir la matriz de eigen desde js

--- source/edf_bindings.cpp
+++ source/edf_bindings.cpp
@@ -1,12 +1,24 @@
 #include <emscripten/bind.h>
 #include "edfwasm.hpp"
-#include <vector>
+#include "signaldata.hpp"
+#include "../Eigen/Dense"
 EMSCRIPTEN_BINDINGS(edf_module) {
-    
+    emscripten::register_vector<Eigen::VectorXcd>("VectorXcdVector");
     emscripten::class_<edf>("EDF")
         .constructor<const std::string&>()
         .function("PrintHeaderRecords", &edf::PrintHeaderRecords)
         .function("PrintDataRecords", &edf::PrintDataRecords)
         .function("PrintSizeSignals", &edf::PrintSizeSignals)
-        .function("PrintTopValues", &edf::PrintTopValues);
+        .function("PrintTopValues", &edf::PrintTopValues)
+        .property("Signals", &edf::Signals);
+
+        emscripten::class_<SignalData>("SignalData")
+        .constructor<>()
+        .function("CalculateMeans", &SignalData::CalculateMeans)
+        .function("CalculateDeviation", &SignalData::CalculateDeviation)
+        .function("PrintMeanAndDeviation", &SignalData::PrintMeanAndDeviation)
+        .property("signals", &SignalData::Signals);
+
+        emscripten::class_<Eigen::VectorXcd>("EigenVector");
+        
 }
